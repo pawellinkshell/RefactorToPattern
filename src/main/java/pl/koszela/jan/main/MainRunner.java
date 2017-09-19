74afdf0268aaa510d84c1d01efd2e7789fc38356
Jan Koszela <jan.koszela@hicron.com>
2017-09-19 12:29:44 +0200

    Add Hello World main class runner

---
+++ src/main/java/pl/koszela/jan/main/MainRunner.java
@@ -0,0 +1,14 @@
+package pl.koszela.jan.main;
+
+/**
+ * Created on 19.09.2017.
+ *
+ * @author Jan Koszela
+ */
+public class MainRunner {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+
+}
